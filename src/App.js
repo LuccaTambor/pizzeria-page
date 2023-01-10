f6414fbc59ff3bc1426ae624e3c6b86ccff76982
LuccaTambor <lucca.tambor@outlook.com>
2023-01-10 11:14:05 -0300

    feat: app building

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FaPizzaSlice } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Hero from 'components/molecules/Hero/Hero';
+import Heading from 'components/atoms/Heading/Heading';
+import Button from 'components/atoms/Button/Button';
+import ThemeProvider from 'styles/ThemeProvider';
+import GlobalStyle from 'styles/GlobalStyle';
+import Feature from "components/atoms/Feature/Feature";
+import Grid from "components/atoms/Grid/Grid";
+
+import PizzaBg from 'assets/pizzaBg.jpg';
+
+const App = () => (
+  <ThemeProvider>
+    <GlobalStyle />
+    <Hero image={PizzaBg}>
+    <Heading>
+      <h1>The best pizza you will ever eat</h1>
+    </Heading>
+    <ul>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+    </ul>
+    <Button color='primary' variant='outlined'>Order now</Button>
+    </Hero>
+    <Grid sm={2} md={4}>
+      <Feature icon={<FaPizzaSlice />} title="title">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaPizzaSlice />} title="title">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaPizzaSlice />} title="title">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </Feature>
+      <Feature icon={<FaPizzaSlice />} title="title">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      </Feature>
+    </Grid>
+  </ThemeProvider>
+)
 
 export default App
