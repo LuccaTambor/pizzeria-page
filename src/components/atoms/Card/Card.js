86c18b028ecd841702fb2370a481d909d3c0d590
LuccaTambor <lucca.tambor@outlook.com>
2023-01-10 18:39:01 -0300

    feat: card component

---
+++ src/components/atoms/Card/Card.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+
+const Root = styled.div`
+  background-color: #fff;
+  border-radius: 4px;
+`;
+
+const StyledBody = styled.div`
+  padding: 16px;
+
+  h6 {
+    margin-top: 0;
+  }
+`;
+
+export const CardBody = ({ children }) => (
+  <StyledBody>
+    {children}
+  </StyledBody>
+);
+
+const StyledMedia = styled.div`
+  background-image: url(${props => props.image});
+  background-position: center center;
+  background-size: cover;
+  height: 270px;
+`;
+
+export const CardMedia = ({ image}) => (
+  <StyledMedia image={image}>
+  </StyledMedia>
+);
+
+const Card = ({ children }) => (
+  <Root>
+    {children}
+  </Root>
+);
+
+Card.defaultProps = {};
+
+Card.propTypes = {};
+
+export default Card;
---
+++ src/components/atoms/Card/Card.stories.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Card, { CardBody, CardMedia  } from "./Card";
+import Section from "components/molecules/Section/Section";
+import Heading from "components/atoms/Heading/Heading";
+import Button from "components/atoms/Button/Button";
+
+import FlavourImage from "stories/assets/margherita.jpg";
+
+export default {
+  title: "Components/Atoms/Card",
+  component: Card
+};
+
+export const usage = () => (
+  <Section inverse>
+    <Card>
+      <CardBody>
+        <Heading>
+          <h6>Título</h6>
+        </Heading>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae veritatis molestias provident magni aut mollitia.</p>
+        <div>
+          <Button variant="link" color="primary">Know more</Button>
+        </div>
+      </CardBody>
+    </Card>
+  </Section>
+);
+
+export const withMedia = () => (
+<Section inverse>
+    <Card>
+      <CardMedia image={FlavourImage}/>
+      <CardBody>
+        <Heading>
+          <h6>Título</h6>
+        </Heading>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae veritatis molestias provident magni aut mollitia.</p>
+        <div>
+          <Button variant="link" color="primary">Know more</Button>
+        </div>
+      </CardBody>
+    </Card>
+  </Section>
+
+);
